a22876d25011e8b4485d11e2c1d78fd8a7cd0b48
leeseongbok <lsb@j-sol.co.kr>
2023-05-22 18:19:22 +0900

    [FORMAT] lint formatting

--- api/src/main/kotlin/com/github/kimleepark2/api/rest/FileUploadRest.kt
+++ api/src/main/kotlin/com/github/kimleepark2/api/rest/FileUploadRest.kt
@@ -3,20 +3,17 @@ package com.github.kimleepark2.api.rest
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import java.io.IOException
 
 @RestController
 @RequestMapping("/api/files")
