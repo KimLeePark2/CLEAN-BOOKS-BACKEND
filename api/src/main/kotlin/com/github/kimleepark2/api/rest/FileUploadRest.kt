3f12fd2c86abaf8aca777cb41eb0dab770405125
이성복 <dltjdqhr1000@gmail.com>
2023-05-23 01:51:03 +0900

    [FEAT] S3 파일 테스트 등록 API 수정

--- api/src/main/kotlin/com/github/kimleepark2/api/rest/FileUploadRest.kt
+++ api/src/main/kotlin/com/github/kimleepark2/api/rest/FileUploadRest.kt
@@ -2,19 +2,22 @@ package com.github.kimleepark2.api.rest
 
 import com.github.kimleepark2.common.aws.AwsS3Uploader
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import kotlin.math.log
 
 @RestController
 @RequestMapping("/api/files")
@@ -22,9 +25,7 @@ import org.springframework.web.multipart.MultipartFile
 class FileUploadRest(
     private val awsS3Uploader: AwsS3Uploader,
 ) {
-
-    @Value("\${cloud.aws.s3.bucket}")
-    private val bucket: String? = null
+    private val log = LoggerFactory.getLogger(FileUploadRest::class.java)
 
     @ApiResponses(
         ApiResponse(responseCode = "200", description = "성공"),
@@ -33,13 +34,19 @@ class FileUploadRest(
     @Operation(
         summary = "S3 파일 업로드(테스트 기능)",
         description = "단일 파일 업로드 테스트",
-
     )
-    @PostMapping("", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun uploadFile(
-        @Parameter(name = "file", description = "Multipartfile") @RequestParam("file") file: MultipartFile,
-    ): String {
-        return awsS3Uploader.upload(file, "test")
+        @RequestParam("file") file: MultipartFile,
+    ) : FileResponse {
+//    : ResponseEntity<String> {
+//        val path = awsS3Uploader.upload(file, "test").trimIndent().trim()
+//        log.info("path: $path")
+//        return ResponseEntity.ok().body(path)
+        return FileResponse(awsS3Uploader.upload(file, "test").trimIndent().trim())
     }
+
+    inner class FileResponse(
+        val path: String,
+    )
 }
