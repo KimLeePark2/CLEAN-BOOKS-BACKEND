1f59097dbc4be060cf6280f3cc4e7b07354886bd
leeseongbok <lsb@j-sol.co.kr>
2023-05-22 18:12:00 +0900

    [FEAT] lombok 제거

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,8 +4,6 @@ dependencies {
     implementation(project(":domain"))
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    implementation("org.projectlombok:lombok:1.18.22")
 }
 
 // bootJar 허용, 실행 jar 만들기
