baaad446c6f2bea91414c26e4d7a22ee3ea496d2
이성복 <dltjdqhr1000@gmail.com>
2023-05-23 01:54:50 +0900

    Revert "[FORMAT] lint"
    This reverts commit 6ffaf0e7e1fa0b9a4273738cab0b76748d854319.

--- api/src/main/kotlin/com/github/kimleepark2/api/rest/FileUploadRest.kt
+++ api/src/main/kotlin/com/github/kimleepark2/api/rest/FileUploadRest.kt
@@ -6,12 +6,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import kotlin.math.log
 
 @RestController
 @RequestMapping("/api/files")
@@ -32,7 +38,7 @@ class FileUploadRest(
     @PostMapping("", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun uploadFile(
         @RequestParam("file") file: MultipartFile,
-    ): FileResponse {
+    ) : FileResponse {
 //    : ResponseEntity<String> {
 //        val path = awsS3Uploader.upload(file, "test").trimIndent().trim()
 //        log.info("path: $path")
