5d3adc51fbd9beb17c9f8da94ec3378284d8ac56
leeseongbok <lsb@j-sol.co.kr>
2023-06-05 10:58:15 +0900

    [CHORE] 의존성 수정

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,15 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
+    implementation(project(":common"))
     implementation(project(":domain"))
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("org.springframework.security:spring-security-test:5.7.3")
+}
+
+tasks.withType<Jar> {
+    enabled = false
 }
 
 // bootJar 허용, 실행 jar 만들기
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -29,41 +29,19 @@ dependencies {
 
     val kapt by configurations
 
-    // // spring-boot-starter
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    api("org.springframework.boot:spring-boot-starter-data-jpa")
-    api("org.springframework.boot:spring-boot-starter-security")
-//    implementation("org.springframework.boot:spring-boot-starter-security")
-//    implementation("org.springframework.boot:spring-boot-starter-actuator:3.0.1")
-
-    // javax.servlet 사용을 위한 라이브러리 - filter에서 사용 9버전까지 사용 가능하며 10버전 이후론 삭제된듯하다.
-    implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.65")
-
-    api("org.springdoc:springdoc-openapi-ui:1.6.14")
-    api("org.springdoc:springdoc-openapi-kotlin:1.6.14")
-
-    // jwt
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-    implementation("io.jsonwebtoken:jjwt-gson:0.11.5") // implementation을 api로 변경하면 오류가 발생한다??
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5") // implementation을 api로 변경하면 오류가 발생한다??
-
+    // Q CLASS 생성을 위해 필요
     kapt("org.springframework.boot:spring-boot-configuration-processor")
 
-    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.8.1")
-
-
     // kotest
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-property:$kotestVersion")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
-    testImplementation("io.kotest:kotest-framework-datatest:5.3.0")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testApi("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testApi("io.kotest:kotest-assertions-core:$kotestVersion")
+    testApi("io.kotest:kotest-property:$kotestVersion")
+    testApi("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    testApi("io.kotest:kotest-framework-datatest:5.3.0")
+    testApi("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 
     // kotlin-faker
-    testImplementation("io.github.serpro69:kotlin-faker:1.11.0")
+    testApi("io.github.serpro69:kotlin-faker:1.11.0")
 }
 
 tasks.withType<Jar> {
