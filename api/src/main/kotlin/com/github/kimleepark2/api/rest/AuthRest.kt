0daeec50412d4650e268f34e7bb425bdd60fedad
이성복 <dltjdqhr1000@gmail.com>
2023-06-11 13:23:19 +0900

    [FORMAT] lint

--- api/src/main/kotlin/com/github/kimleepark2/api/rest/AuthRest.kt
+++ api/src/main/kotlin/com/github/kimleepark2/api/rest/AuthRest.kt
@@ -3,7 +3,6 @@ package com.github.kimleepark2.api.rest
 import com.github.kimleepark2.domain.entity.user.UserService
 import com.github.kimleepark2.domain.entity.user.UserServiceImpl
 import com.github.kimleepark2.domain.entity.user.dto.request.LoginRequest
-import com.github.kimleepark2.domain.entity.user.dto.request.UserCreateRequest
 import com.github.kimleepark2.domain.entity.user.dto.response.LoginResponse
 import com.github.kimleepark2.domain.entity.user.dto.response.UserResponse
 import io.swagger.v3.oas.annotations.Operation
